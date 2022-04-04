2d570b63b46d410da1c3c89d8f54bf790edbdd5c
Haukaun <haakonfs@hotmail.com>
2022-04-05 00:13:17 +0200

    initial commit for react and some navbar testing.

---
+++ frontend-react/src/App.js
@@ -0,0 +1,16 @@
+import './styles/global.css';
+
+import { Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav.jsx";
+
+function App() {
+    return (
+        <div>
+            <Routes>
+                <Route path="/" element={<Nav/>}/>
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
