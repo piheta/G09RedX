abbf02019ac1370379056508c26d17ff087b7ad1
PMolnes <pettermolnes@icloud.com>
2022-04-11 14:33:35 +0200

    Fixed a scrolling bug and added an error message on failed login.
    Fixed a scrolling bug related to the hamburger menu where the scrolling wasn't enabled again after pressing on login and going back to the main page.

--- frontend-react/src/components/login/Login.jsx
+++ frontend-react/src/components/login/Login.jsx
@@ -14,6 +14,8 @@ function Login() {
     const [username, setUsername] = useState("");
     const dispatch = useDispatch();
     const isLogged = useSelector(state => state.isLogged);
+    const [warning, setWarning] = React.useState(false);
+    const [warningText, setWarningText] = React.useState("");
 
     function sendLoginRequest() {
         let loginInfo = {
@@ -34,13 +36,17 @@ function Login() {
         }).then((response) => {
             console.log(response.data);
             if (response.status === 200) {
+                setWarning(false);
                 dispatch(setLoginStatus({
                     isLogged: true,
                     jwToken: response.data.jwt
                 }));
                 navigate("/");
             }
-        }).catch((response) => console.log(response.status));
+        }).catch((error) => {
+            setWarning(true);
+            setWarningText(error.response.data);
+        })
     };
 
     return (
@@ -53,6 +59,17 @@ function Login() {
                     <TextField onChange={(elem) => setUsername(elem.target.value)}
                                label="Username" type={"text"} margin={"dense"} variant={"filled"} color={"error"}/>
                     <TextField onChange={(elem) => setPassword(elem.target.value)} label="Password" type={"text"} margin={"dense"} variant={"filled"} color={"error"}/>
+
+                    {
+                        (warning === true)
+                            ?
+                            <div className={"sign-up-warning"}>
+                                <p>Warning: {warningText}</p>
+                            </div>
+                            :
+                            <div></div>
+                    }
+
                     <Button onClick={sendLoginRequest} variant="outlined" color={"error"}>Confirm</Button>
                 </div>
 
