685eb275ac234e27c5f92a54463dceabff4b18dd
Haukaun <haakonfs@hotmail.com>
2022-05-24 00:25:54 +0200

    Made Modal for add review. deleted useless classes.

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -2,11 +2,9 @@ import './styles/global.css';
 
 import {Routes, Route} from "react-router-dom";
 import HomePage from "./pages/Homepage";
-import OneDayCourse from "./pages/OneDayCourse";
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
-import TwoDayCourse from "./pages/TwoDayCourse";
-import ShortConsultation from "./pages/ShortConsultation";
+
 
 
 function App() {
@@ -14,9 +12,6 @@ function App() {
         <div>
             <Routes>
                 <Route exact path="/" element={<HomePage />}/>
-                <Route path="/checkout/oneDayCourse" element={<OneDayCourse />}/>
-                <Route path="/checkout/twoDayCourse" element={<TwoDayCourse />}/>
-                <Route path="/checkout/shortconsultation" element={<ShortConsultation />}/>
                 <Route path ="/login" element = {<Login />}/>
                 <Route path ="/register" element = {<Register />}/>
             </Routes>
---
+++ frontend-react/src/components/reviewSection/ReviewSection.jsx
@@ -0,0 +1,87 @@
+import React, {useEffect, useRef, useState} from 'react';
+import Review from "../review/Review";
+import "./ReviewSection.css"
+import {Button} from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import Rating from "@mui/material/Rating";
+
+
+
+
+function ReviewSection({parentRef}) {
+
+    let reviews = []
+    for (let i = 0; i < 6; i++){
+        reviews.push({
+            author: "arne per",
+            reviewTitle: "",
+            description: "I like this betiful website it is honestly the best ever",
+            star: 4
+        })
+    }
+
+
+    const [state, setState] = useState(false);
+
+
+
+    let ref = useRef();
+
+
+    const handleClickOutside = (event) => {
+        if (parentRef.current && parentRef.current.contains(event.target) && !ref.current.contains(event.target)) {
+            setState(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside)
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [parentRef]);
+
+
+    return (
+        <section id={"review-section"}>
+            <div className={"review-section-header"}>
+                <label className={"review-section-label"}>Customer reviews</label>
+                { state === false ?
+                    <Button onClick={() => setState(!state)} variant="outlined" color={"error"}>Add review</Button>
+                    : null
+                }
+            </div>
+            { state === true ?
+                <div className="modal">
+                    <div className ="modal-content" ref={ref}>
+                        <div className={"modal-wrapper"}>
+                            <label className={"modal-title"}>Add Review</label>
+                            <label className={"modal-username"}>Username</label>
+                            <Rating
+                                name="customized-color"
+                                defaultValue={3}
+                                getLabelText={(value) => `${value} Heart${value !== 1 ? 's' : ''}`}
+                                precision={1}
+                                icon={<FavoriteIcon sx={{color: "#ec361e"}} fontSize="inherit" />}
+                                emptyIcon={<FavoriteBorderIcon sx={{color: "#ec361e"}} fontSize="inherit" />}
+                                size={"large"}
+                            />
+                            <textarea className={"modal-text"} />
+                            <Button className={"modal-button"} variant="outlined" color={"error"}>Add review</Button>
+                        </div>
+                    </div>
+                </div>
+                : null
+            }
+            <hr />
+            <div className={"review-section-review-list"}>
+                {
+                    reviews.map((review) => <Review userName={review.author} comment={review.description} rating={review.star} />)
+                }
+            </div>
+        </section>
+    );
+}
+
+export default ReviewSection;
