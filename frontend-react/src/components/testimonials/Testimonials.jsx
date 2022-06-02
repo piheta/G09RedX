97e19b2584c5a4fc64365dbe742d4d3bad2af2e5
tama3ti <m.picheta@icloud.com>
2022-06-02 18:25:28 +0200

    made the styling more uniform and fixed heading decendation

--- frontend-react/src/components/testimonials/Testimonials.jsx
+++ frontend-react/src/components/testimonials/Testimonials.jsx
@@ -8,23 +8,23 @@ function Testimonials() {
         <section id="testimonial-section">
             <div className="testimonial-header">
                 <h1>Testimonials</h1>
-                <h2>Reviews from our happiest customers </h2>
+                <h2>Reviews from our happiest customers</h2>
             </div>
 
 
             <div className="testimonial-wrapper">
                 <div className="testimonial">
                     <img src={ "/images/user1.png" } alt=""/>
-                    <h2>EXCELLENT EXPERIENCE</h2>
+                    <h3>Truly Amazing</h3>
                     <p>
                         "Red Cross are very competent experts in first help.
-                            They are so effective; you never need the second help."
+                        They are so effective; you never need the second help."
                     </p>
                 </div>
 
                 <div className="testimonial">
                     <img src={ "/images/user2.png" } alt=""/>
-                    <h2>GREAT SERVICE</h2>
+                    <h3>Great Service</h3>
                     <p>
                         "I have been HR responsible at my company for 12 years, and every year I update my certification with Red Cross Ålesund.
                         I can proudly say that no one has died under my watch."
@@ -32,11 +32,11 @@ function Testimonials() {
                 </div>
                 <div className="testimonial">
                     <img src={ "/images/user3.png" } alt=""/>
-                        <h2>EXCELLENT EXPERIENCE</h2>
-                            <p>
-                                "These folks may not be Linux kernel gurus, but they do one thing well –
-                                they teach you the essentials of first help in an easy and interesting way."
-                            </p>
+                    <h3>Superb Experience</h3>
+                    <p>
+                        "These folks may not be Linux kernel gurus, but they do one thing well –
+                        they teach you the essentials of first help in an easy and interesting way."
+                    </p>
                 </div>
             </div>
         </section>
