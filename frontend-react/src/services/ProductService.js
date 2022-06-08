dc89ada6cff60fd4fb3141030c772b8453609607
Haukaun <haakonfs@hotmail.com>
2022-06-08 22:10:06 +0200

    Removed unused function in service class.

--- frontend-react/src/services/ProductService.js
+++ frontend-react/src/services/ProductService.js
@@ -22,18 +22,4 @@ export function getAllProducts() {
     })
 };
 
-export async function getProductById(productId) {
-    return await axios({
-        method: 'get',
-        url: API_BASE_URL + '/products/' + productId,
-        headers: {
-            'Accept': '*/*',
-            "Content-Type": "application/json"
-        },
-    }).then((response) => {
-        if (response.status === 200) {
-            return response.data;
-        }
-    })
-};
 
